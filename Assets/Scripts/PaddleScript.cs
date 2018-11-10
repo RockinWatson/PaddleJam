ee1873f25ab5039ffe382763ad0eb9b573162340
Joshua Watson <josh.m.watson@gmail.com>
2018-11-10 16:44:58 -0600

    added puck and paddle physics

---
+++ Assets/Scripts/PaddleScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class PaddleScript : MonoBehaviour {
+
+    public Rigidbody2D RB;
+    public float Speed;
+    public float MaxX;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var xInput = Input.GetAxis("Horizontal");
+
+        if (xInput == 0)
+        {
+            Stop();
+        }
+        if (xInput < 0)
+        {
+            MoveLeft();
+        }
+        if (xInput > 0)
+        {
+            MoveRight();
+        }
+
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Clamp(pos.x, -MaxX, MaxX);
+        transform.position = pos;
+    }
+
+    void MoveLeft() {
+        RB.velocity = new Vector2(-Speed, 0);
+    }
+
+    void MoveRight(){
+        RB.velocity = new Vector2(Speed,0);
+    }
+
+    void Stop() {
+        RB.velocity = Vector2.zero;
+    }
+}
---
+++ Assets/Scripts/PuckScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PuckScript : MonoBehaviour {
+
+    public Rigidbody2D RB;
+    public float BallForce;
+
+	// Use this for initialization
+	void Start () {
+        RB.AddForce(new Vector2(BallForce, BallForce));
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
