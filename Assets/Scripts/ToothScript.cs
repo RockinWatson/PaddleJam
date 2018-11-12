f2ce32789308477c9c2f8647f820de6d67d18cf5
Joshua Watson <josh.m.watson@gmail.com>
2018-11-12 15:32:32 -0600

    teeth degrade on hit

--- Assets/Scripts/ToothScript.cs
+++ Assets/Scripts/ToothScript.cs
@@ -2,13 +2,18 @@
 
 public class ToothScript : MonoBehaviour {
 
+    public SpriteRenderer _spRend;
+    public Sprite[] _sprites;
+
+    private int _health = 3;
+
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () {   
 		
 	}
 
@@ -16,7 +21,23 @@ public class ToothScript : MonoBehaviour {
     {
         if (collision.gameObject.tag == "puck")
         {
-            gameObject.SetActive(false);
+            _health -= 1;
+            if (_health <= 0)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void OnGUI()
+    {
+        if (_health == 2)
+        {
+            _spRend.sprite = _sprites[1];
+        }
+        if (_health == 1)
+        {
+            _spRend.sprite = _sprites[2];
         }
     }
 }
