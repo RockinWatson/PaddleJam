4a5dee82db4fffa2360b5d6b9abdeb4895f60b56
tweedle <tsdunn86@gmail.com>
2018-11-12 18:37:39 -0600

    puck release sound

--- Assets/Scripts/PuckScript.cs
+++ Assets/Scripts/PuckScript.cs
@@ -4,13 +4,20 @@ public class PuckScript : MonoBehaviour {
 
     public Rigidbody2D RB;
     public float BallForce;
+	public AudioClip PuckRelease;
 
     private bool _gamestart = false;
+	private AudioSource source;
+	private float volLowRange = .5f;
+	private float volHighRange = 1.0f;
 
 	// Use this for initialization
 	void Start () {
         	
+		source = GetComponent<AudioSource>();
 	}
+
+
 	
 	// Update is called once per frame
 	void Update () {
@@ -21,6 +28,10 @@ public class PuckScript : MonoBehaviour {
 
             RB.AddForce(new Vector2(BallForce, BallForce));
             _gamestart = true;
-        }
+       
+			float vol = Random.Range (volLowRange, volHighRange);
+			source.PlayOneShot(PuckRelease,vol);
+
+		}
     }
 }
